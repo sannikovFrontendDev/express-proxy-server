31802512351607333e6a109183cb40f227864ec7
AlekseiSannikov <sannikov.frontend@gmail.com>
2025-07-30 16:06:40 +0200

    update .env

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(
   })
 );
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🔁 Прокси-сервер слушает на http://localhost:${PORT}`);
 });
